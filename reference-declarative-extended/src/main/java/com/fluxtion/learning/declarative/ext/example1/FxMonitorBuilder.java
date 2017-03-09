e8cde55ef15bd407a665c78e1a8d76dac67909c3
v12technology <greg.higgins@v12technology.com>
2017-03-09 19:42:30 +0000

    FLUX - updated license

--- reference-declarative-extended/src/main/java/com/fluxtion/learning/declarative/ext/example1/FxMonitorBuilder.java
+++ reference-declarative-extended/src/main/java/com/fluxtion/learning/declarative/ext/example1/FxMonitorBuilder.java
@@ -1,5 +1,5 @@
-/*
- * Copyright (C) 2017 Greg Higgins
+/* 
+ * Copyright (C) 2017 V12 Technology Limited
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
