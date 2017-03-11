acd61bb1100a3065cb26946bac0b7311b59b4ab8
greg <greg.higgins@v12technology.com>
2017-03-11 21:16:25 +0000

    FLUX - changes to due to rebuild

--- reference-core/src/main/java/com/fluxtion/learning/example23/generated/AssetMonitor.java
+++ reference-core/src/main/java/com/fluxtion/learning/example23/generated/AssetMonitor.java
@@ -1,19 +1,3 @@
-/* 
- * Copyright (C) 2017 V12 Technology Limited
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.fluxtion.learning.example23.generated;
 
 import java.util.HashMap;
@@ -73,9 +57,9 @@ public class AssetMonitor implements EventHandler, BatchHandler, Lifecycle {
         if (isDirty_assetHandlerCommodities) {
           notifier.assetBreached(assetHandlerCommodities);
         }
-        if (isDirty_assetHandlerEquities
-            || isDirty_assetHandlerFX
-            || isDirty_assetHandlerCommodities) {
+        if (isDirty_assetHandlerFX
+            || isDirty_assetHandlerCommodities
+            || isDirty_assetHandlerEquities) {
           notifier.onEvent();
         }
         afterEvent();
@@ -85,9 +69,9 @@ public class AssetMonitor implements EventHandler, BatchHandler, Lifecycle {
         if (isDirty_assetHandlerEquities) {
           notifier.assetBreached(assetHandlerEquities);
         }
-        if (isDirty_assetHandlerEquities
-            || isDirty_assetHandlerFX
-            || isDirty_assetHandlerCommodities) {
+        if (isDirty_assetHandlerFX
+            || isDirty_assetHandlerCommodities
+            || isDirty_assetHandlerEquities) {
           notifier.onEvent();
         }
         afterEvent();
@@ -97,9 +81,9 @@ public class AssetMonitor implements EventHandler, BatchHandler, Lifecycle {
         if (isDirty_assetHandlerFX) {
           notifier.assetBreached(assetHandlerFX);
         }
-        if (isDirty_assetHandlerEquities
-            || isDirty_assetHandlerFX
-            || isDirty_assetHandlerCommodities) {
+        if (isDirty_assetHandlerFX
+            || isDirty_assetHandlerCommodities
+            || isDirty_assetHandlerEquities) {
           notifier.onEvent();
         }
         afterEvent();
