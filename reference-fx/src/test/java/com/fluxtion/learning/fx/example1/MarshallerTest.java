b031db10266ac0349497d7e1c0c3c07ef9eba06d
greg <greg.higgins@v12technology.com>
2017-03-31 10:34:59 +0100

    FLUX rf fraud detection WIP, adding test for marshaller

---
+++ reference-fx/src/test/java/com/fluxtion/learning/fx/example1/MarshallerTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2017 Greg Higgins (greg.higgins@V12technology.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.fluxtion.learning.fx.example1;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author Greg Higgins (greg.higgins@V12technology.com)
+ */
+public class MarshallerTest {
+    
+    @Test
+    public void testCsvInput(){
+//        StringDriver.streamChars(
+//                "FxPrice,EURGBP,1.123,1.126\n"
+//                + "FxPrice,EURJPY,112.45,112.67\n"
+//                + "CustomerOrder,32\n"
+//                , new FxAuditCsvMarsahller());
+    }
+}
