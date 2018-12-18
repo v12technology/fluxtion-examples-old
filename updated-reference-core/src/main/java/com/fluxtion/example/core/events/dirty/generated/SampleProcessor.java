57c68627fbfa8ae343a98dde7a0ea82f55b80324
greg higgins <greg.higgins@v12technology.com>
2018-12-18 08:56:44 +0000

    regenerated the solutions

--- updated-reference-core/src/main/java/com/fluxtion/example/core/events/dirty/generated/SampleProcessor.java
+++ updated-reference-core/src/main/java/com/fluxtion/example/core/events/dirty/generated/SampleProcessor.java
@@ -35,11 +35,11 @@ public class SampleProcessor implements EventHandler, BatchHandler, Lifecycle {
   private final MyEventHandler myEventHandler_3 = new MyEventHandler();
   private final DirtyNode dirtyNode_9 = new DirtyNode(myEventHandler_3);
   private final DirtyAggregator dirtyAggregator_11 =
-      new DirtyAggregator(dirtyNode_7, dirtyNode_5, dirtyNode_9);
+      new DirtyAggregator(dirtyNode_9, dirtyNode_7, dirtyNode_5);
   //Dirty flags
+  private boolean isDirty_dirtyNode_5 = false;
   private boolean isDirty_dirtyNode_7 = false;
   private boolean isDirty_dirtyNode_9 = false;
-  private boolean isDirty_dirtyNode_5 = false;
   //Filter constants
 
   public SampleProcessor() {}
@@ -67,7 +67,7 @@ public class SampleProcessor implements EventHandler, BatchHandler, Lifecycle {
     dataEventHandler_1.handleEvent(typedEvent);
     isDirty_dirtyNode_5 = dirtyNode_5.isDirty();
     isDirty_dirtyNode_7 = dirtyNode_7.isDirty();
-    if (isDirty_dirtyNode_7 | isDirty_dirtyNode_5 | isDirty_dirtyNode_9) {
+    if (isDirty_dirtyNode_9 | isDirty_dirtyNode_5 | isDirty_dirtyNode_7) {
       dirtyAggregator_11.publishDirty();
     }
     //event stack unwind callbacks
@@ -78,7 +78,7 @@ public class SampleProcessor implements EventHandler, BatchHandler, Lifecycle {
     //Default, no filter methods
     myEventHandler_3.handleEvent(typedEvent);
     isDirty_dirtyNode_9 = dirtyNode_9.isDirty();
-    if (isDirty_dirtyNode_7 | isDirty_dirtyNode_5 | isDirty_dirtyNode_9) {
+    if (isDirty_dirtyNode_9 | isDirty_dirtyNode_5 | isDirty_dirtyNode_7) {
       dirtyAggregator_11.publishDirty();
     }
     //event stack unwind callbacks
@@ -88,9 +88,9 @@ public class SampleProcessor implements EventHandler, BatchHandler, Lifecycle {
   @Override
   public void afterEvent() {
 
+    isDirty_dirtyNode_5 = false;
     isDirty_dirtyNode_7 = false;
     isDirty_dirtyNode_9 = false;
-    isDirty_dirtyNode_5 = false;
   }
 
   @Override
